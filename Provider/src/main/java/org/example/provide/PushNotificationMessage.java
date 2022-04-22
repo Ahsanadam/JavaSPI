4c5231e4b125486d09dbf9e2178da5334b4e8aab
Ahsanadam <adamebadul@gmail.com>
2022-04-22 15:43:48 +0200

    Add PushNotificationMessage class in Provider module

--- Provider/src/main/java/org/example/provide/PushNotificationMessage.java
+++ Provider/src/main/java/org/example/provide/PushNotificationMessage.java
@@ -3,10 +3,9 @@ package org.example.provide;
 import org.example.message.Message;
 import org.example.message.MessageSender;
 
-public class EmailMessage implements MessageSender {
-
+public class PushNotificationMessage implements MessageSender {
     @Override
     public void sendMessage(Message message) {
-        System.out.println("Sending Email with Message = " + message.message);
+        System.out.println("Sending Push Notification with Message = " + message.message);
     }
 }
