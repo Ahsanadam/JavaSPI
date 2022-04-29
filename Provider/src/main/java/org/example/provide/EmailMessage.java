ba59a69dda1c6581650a44722b1043f84c7339bd
Ahsanadam <adamebadul@gmail.com>
2022-04-29 17:54:24 +0200

    Add annotation on implementations

--- Provider/src/main/java/org/example/provide/EmailMessage.java
+++ Provider/src/main/java/org/example/provide/EmailMessage.java
@@ -1,8 +1,10 @@
 package org.example.provide;
 
+import org.example.message.MessageCategory;
 import org.example.message.Message;
 import org.example.message.MessageSender;
 
+@MessageCategory("Email message")
 public class EmailMessage implements MessageSender {
 
     @Override
--- Provider/src/main/java/org/example/provide/PushNotificationMessage.java
+++ Provider/src/main/java/org/example/provide/PushNotificationMessage.java
@@ -1,8 +1,10 @@
 package org.example.provide;
 
 import org.example.message.Message;
+import org.example.message.MessageCategory;
 import org.example.message.MessageSender;
 
+@MessageCategory("Push notification message")
 public class PushNotificationMessage implements MessageSender {
     @Override
     public void sendMessage(Message message) {
