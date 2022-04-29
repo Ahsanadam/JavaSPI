5c8d11b3f0ba5306f4d658fa173db17a956ab4ca
Ahsanadam <adamebadul@gmail.com>
2022-04-29 17:55:36 +0200

    Add MessageOption methods and refactor Menu class

--- Consumer/src/main/java/Menu.java
+++ Consumer/src/main/java/Menu.java
@@ -1,6 +1,10 @@
 
 import org.example.message.Message;
+import org.example.message.MessageCategory;
 import org.example.message.MessageSender;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 import java.util.ServiceLoader;
 
@@ -13,8 +17,10 @@ public class Menu {
 
         while (true) {
             System.out.println("\nIn which format do you want to write your message?");
-            System.out.println("Write 1, as a push notification message.");
-            System.out.println("Write 2, as an email message.");
+            System.out.print("Write 1, if you want to write it as a ");
+            pushNotificationMessageOption();
+            System.out.print("Write 2, if you want to write it as an ");
+            emailMessageOption();
             System.out.println("Write e, if you want to exit the application.");
             Scanner readUserInput = new Scanner(System.in);
             String choice = readUserInput.nextLine();
@@ -30,36 +36,65 @@ public class Menu {
         }
     }
 
-    private void emailNotificationMessage() {
-        scanner = new Scanner(System.in);
-        System.out.println("Write your message:");
-        String userInput = scanner.nextLine();
-
-        ServiceLoader<MessageSender> serviceLoader = ServiceLoader.load(MessageSender.class);
-        serviceLoader.stream()
-                .map(ServiceLoader.Provider::get)
-                .filter(messageSenderProvider -> messageSenderProvider.getClass().getSimpleName().startsWith("Email"))
-                .forEach(messageSenderProvider -> messageSenderProvider.sendMessage(new Message(userInput)));
-
+    private void emailMessageOption() {
+        getMessageOptions().stream().filter(messageOptions -> messageOptions.startsWith("Email")).forEach(System.out::println);
     }
 
+    private void pushNotificationMessageOption() {
+        getMessageOptions().stream().filter(messageOptions -> messageOptions.startsWith("Push")).forEach(System.out::println);
+    }
+
+
+    private void emailNotificationMessage() {
+        try {
+            scanner = new Scanner(System.in);
+            System.out.println("Write your message:");
+            String userInput = scanner.nextLine();
+
+            ServiceLoader<MessageSender> serviceLoader = ServiceLoader.load(MessageSender.class);
+            serviceLoader.stream()
+                    .map(ServiceLoader.Provider::get)
+                    .filter(messageSenderProvider -> messageSenderProvider.getClass().getSimpleName().startsWith("Email"))
+                    .forEach(messageSenderProvider -> messageSenderProvider.sendMessage(new Message(userInput)));
+        } catch (Exception e) {
+            System.out.println("The format is invalid, try again");
+        }
+
+    }
 
 
     private void pushNotificationMessage() {
-        scanner = new Scanner(System.in);
-        System.out.println("Write your message:");
-        String userInput = scanner.nextLine();
+        try {
+            scanner = new Scanner(System.in);
+            System.out.println("Write your message:");
+            String userInput = scanner.nextLine();
 
-        ServiceLoader<MessageSender> serviceLoader = ServiceLoader.load(MessageSender.class);
-        serviceLoader.stream()
-                .map(ServiceLoader.Provider::get)
-                .filter(messageSenderProvider -> messageSenderProvider.getClass().getSimpleName().startsWith("Push"))
-                .forEach(messageSenderProvider -> messageSenderProvider.sendMessage(new Message(userInput)));
+            ServiceLoader<MessageSender> serviceLoader = ServiceLoader.load(MessageSender.class);
+            serviceLoader.stream()
+                    .map(ServiceLoader.Provider::get)
+                    .filter(messageSenderProvider -> messageSenderProvider.getClass().getSimpleName().startsWith("Push"))
+                    .forEach(messageSenderProvider -> messageSenderProvider.sendMessage(new Message(userInput)));
+        } catch (Exception e) {
+            System.out.println("The format is invalid, try again");
+        }
 
     }
 
 
-    private void applicationGreeting() {
+
+    private List<String> getMessageOptions() {
+        ServiceLoader<MessageSender> serviceLoader = ServiceLoader.load(MessageSender.class);
+        List<String> list = new ArrayList<>();
+
+        for (MessageSender messageSender : serviceLoader) {
+            MessageCategory annotation = messageSender.getClass().getAnnotation(MessageCategory.class);
+            list.add(annotation.value());
+        }
+        return list;
+    }
+
+
+    private void applicationGreeting () {
 
         System.out.println(
                 """
@@ -70,5 +105,4 @@ public class Menu {
 
     }
 }
-
 
