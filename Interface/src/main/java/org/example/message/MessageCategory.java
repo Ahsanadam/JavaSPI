3e29c7f3fa9c404aafeb98b7f05b5f7814f503f4
Ahsanadam <adamebadul@gmail.com>
2022-04-29 17:52:53 +0200

    Add MessageCategory annotation

---
+++ Interface/src/main/java/org/example/message/MessageCategory.java
@@ -0,0 +1,12 @@
+package org.example.message;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.TYPE})
+public @interface MessageCategory {
+    String value() default "empty";
+}
